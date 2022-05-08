d0d3e4588230a3a8184c7f51ce4e3de0aa9ec18b
FaizaManadir8 <you@example.com>
2022-05-08 20:59:19 +0100

    back

---
+++ src/test/java/pfe/gestionDeConges/backen/ApplicationTests.java
@@ -0,0 +1,13 @@
+package pfe.gestionDeConges.backen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
